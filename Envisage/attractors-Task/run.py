9760da362c0d7838d4d8243a456fe9ab1a92b12a
Jonathan Rocher <jrocher@enthought.com>
2011-07-13 14:36:35 -0500

    Added missing files to this example

--- Envisage/attractors-Task/run.py
+++ Envisage/attractors-Task/run.py
@@ -1,6 +1,15 @@
 # Standard library imports.
 import logging
 
+## Toolkit
+# Line below added to make sure enthought.qt.__init__ is run early enough 
+# to avoid conflicts with other imports.
+import enthought.qt
+from enthought.etsconfig.api import ETSConfig
+ETSConfig.toolkit='qt4'
+QT_API = 'pyqt'
+#QT_API = 'pyside'
+
 # Plugin imports.
 from envisage.core_plugin import CorePlugin
 from envisage.ui.tasks.tasks_plugin import TasksPlugin
