fc35872701c08e243468b2a0e656384a302506fc
Jonathan Rocher <jrocher@enthought.com>
2011-09-04 12:17:26 -0500

    Some clean ups

--- json/json_start.py
+++ json/json_start.py
@@ -14,7 +14,6 @@ class my_class(object):
 def my_class_json_dumphook(z):
     """ Create a hook to be able to serialize my_class type objects
     """
-    #return '{"class": "%s", "value": %s}' % (z.__class__, z.a)
     return {"class": str(z.__class__), "value": z.a}
 
 def my_class_json_loadhook(in_dict):
@@ -25,7 +24,7 @@ def my_class_json_loadhook(in_dict):
         return my_class(a = in_dict["value"])
 
 def explore_dumping(x, y, z, var_in_file, filename):
-    """
+    """ Test dumping the object to a string and a file
     """
     print json.dumps(x)
     print json.dumps(y)
@@ -47,7 +46,8 @@ def explore_dumping(x, y, z, var_in_file, filename):
     return 
 
 def explore_loading(filename):
-    
+    """ Test loading a non-standard object from a file
+    """
     f = open(filename, "r")
     try:
         new_z = json.load(f, object_hook = my_class_json_loadhook)
@@ -64,9 +64,7 @@ if __name__ == "__main__":
     z.increase_a()
 
     filename = "json_start.cfg"
-
-    explore_dumping(x, y, z, z, filename)
-    
+    explore_dumping(x, y, z, z, filename)    
     new_z = explore_loading(filename)
 
     print "Are the 2 objects equal?", z == new_z
