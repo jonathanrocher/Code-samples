5807a40d150e57b614d0e0ac44b26c2a79b9ac95
Jonathan Rocher <jrocher@enthought.com>
2011-09-08 12:23:03 -0500

    Adding an example of pickle usage

---
+++ pickle/pickle.py
@@ -0,0 +1,27 @@
+import pickle
+
+class MyInfo(object):
+    name = "Jonathan Rocher"
+    age = 31
+    Address = {"street": "Rip Ford Dr",
+               "Number": 3614,
+               "City": "Austin"}
+    
+def save2file(object, filename):
+    file = open(filename,'wb')
+    pickle.dump(object, file, protocol = 2)
+    file.close()
+    
+def recover_from_file(filename):
+    file = open(filename,'rb')
+    obj = pickle.load(file)
+    file.close()
+    return obj
+
+
+if __name__ == "__main__":
+    info = MyInfo()
+    filename = "myinfo.pkl"
+    save2file(info,filename)
+    new_obj = recover_from_file(filename)
+    print "object recovered is", new_obj
