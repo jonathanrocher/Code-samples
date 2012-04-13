26501e3c6ceaca732fe4f120f3fc7f365ebe38c7
Jonathan Rocher <jrocher@enthought.com>
2012-04-12 19:34:45 -0500

    Added lookup to large database, and memoization into local small json file.

--- matplotlib/basemap_intro.py
+++ matplotlib/basemap_intro.py
@@ -3,50 +3,103 @@
 
 Author: Jonathan Rocher, Enthought Inc.
 """
-from mpl_toolkits import basemap
-from matplotlib.pyplot import figure, subplot, title, show, savefig, text
+import os, json, warnings
+
 import numpy as np
 from numpy.testing import assert_
-
+from mpl_toolkits import basemap
+from matplotlib.pyplot import figure, subplot, title, show, savefig, text
 
 cylindrical_projection_list = ['mill', 'cyl', 'merc', 'gall']
 pseudocylindrical_projection_list = ['ortho', 'moll', 'sinu', 'aeqd']
 conical_projection_list = ['lcc', 'eqdc', 'aea']
 
+# local mini db to get started. Add more entries manually using 
+# http://www.travelmath.com/cities/
+# Everytime a lookup is made in the large db, the local one is extended with the 
+# result.
 city_db = {# USA
-               "austin, tx": ((30, 16, 0, 'N'),(97, 44, 34, 'W')), 
-               "boulder, co": ((40, 0, 54, 'N'),(105, 16, 12, 'W')), 
-               "new york, ny": ((40, 42, 51, 'N'),(74, 0, 23, 'W')),
+               "austin, tx, us": ((30, 16, 0, 'N'),(97, 44, 34, 'W')), 
+               "boulder, co, us": ((40, 0, 54, 'N'),(105, 16, 12, 'W')), 
+               "new york, ny, us": ((40, 42, 51, 'N'),(74, 0, 23, 'W')),
 
            # Europe
-               "paris, fr": ((48, 52, 0, 'N'),(2, 20, 0, 'E')), 
-               "london, uk": ((51, 30, 0, 'N'),(0, 7, 0, 'W'))
+               "paris, , fr": ((48, 52, 0, 'N'),(2, 20, 0, 'E')), 
+               "london, , uk": ((51, 30, 0, 'N'),(0, 7, 0, 'W'))
                }
+
 
 def deg_min_sec2deg(loc = (0,0,0,'N')):
     """ Convert a lat or lon in (deg, min, sec, orientation) into a decimal 
-    degree value inside [-180, 180] for longitudes and [-90, 90] for latitudes.
+    degree value.
     """
+    # Deal with a sequence of locations
     if isinstance(loc[0], tuple):
         return [deg_min_sec2deg(loc[i]) for i in range(len(loc))]
     
     (degs, mins, secs, orientation) = loc
-    if orientation in ['N', 'S']:
-        loc_type = 'lat'
-    elif orientation in ['E', 'W']:
-        loc_type = 'lon'
-    else:
-        raise ValueError("The orientation should be 'N', 'S', 'E', 'W' but %s "
-                         "was passed." % orientation)
-    print "Coordinate type:", loc_type
     
     if orientation in ['W', 'S']:
         sign = -1
+    elif orientation in ['E', 'N']:
+        sign = 1
     else:
-        sign = 1 
+        raise ValueError("The orientation should be 'N', 'S', 'E', 'W' but %s "
+                         "was passed." % orientation)
     
     result = sign * (degs + mins/60. + secs / 3600.)
     return result
+
+def load_world_city_db():
+    """ Load the world city db from http://www.maxmind.com/download/worldcities/
+    """
+    try:
+        db = np.genfromtxt("worldcities.txt", delimiter = ',', names = True, 
+                            dtype = None)
+    except IOError as e:
+        raise IOError("File not found: download the file from "
+                      "http://www.maxmind.com/download/worldcities/")
+    return db
+
+def get_coordinates_city(city = "Austin", state = "", country = "US"):
+    """ Convert a city name into a pair of coordinates. The state is optional 
+    and can be given the value of None, or "".
+    Everytime a city is looked up in the big db, the result is stored in the 
+    local one.
+    """
+    global city_db
+    local_db_file = "local_city_db.json"
+    if os.path.isfile(local_db_file):
+        with open(local_db_file, "r") as f:
+            city_db = json.load(f)
+    try:
+        lat, lon = city_db["%s, %s, %s" % (city.lower(), state.lower(), 
+                                           country.lower())]
+    except KeyError as e:
+        # City not in the local dict. Attempt to load the db from the file into 
+        # a structured array
+        print "Attempting to search the db for city:", city, state, country
+        world_city_db = load_world_city_db()
+        if len(world_city_db) > 1:
+            crit = ((world_city_db['City'] == city.lower()) 
+                    & (world_city_db['Country'] == country.lower()) 
+                    & (world_city_db['Region'] == state.upper()))
+            index = np.where(crit)[0]
+            if len(index) != 1:
+                raise ValueError("The number of cities with the criteria was "
+    	                         "%s. Cannot proceed." % len(index))
+            lon, lat = world_city_db[index]['Longitude'][0], world_city_db[index]['Latitude'][0]
+            del world_city_db
+            city_db["%s, %s, %s" % (city.lower(), state.lower(), country.lower())] = (lat,lon)
+            with open(local_db_file, "w") as f:
+                json.dump(city_db, f)
+            
+        else:
+            del world_city_db
+            raise ValueError("Location not found in any database.")
+            
+    return lon, lat
+
 
 def get_crnr_coordinates(region = "world"):
     """ Set the lower left and uper right corner coordinates used in a 
@@ -88,12 +141,6 @@ def get_conical_boundaries(region = "usa"):
         
     return lat_0, lon_0, lat_1, lat_2, width, height
 
-def get_coordinates_city(city = "austin"):
-    """ Convert a city name into a pair of coordinates.
-    TODO.
-    """               
-    lat, lon = city_db.get(city.lower(), (None,None))
-    return lon, lat
 
 def explore_basemap_proj(region = "world", center = (30, -98), 
                          projection_type = "cyl", rolling_proj = True, 
@@ -201,7 +248,7 @@ def add_point_data(bmp, x = [], y = [], lat = [], lon = [], style = 'ro',
     bmp.plot(x,y,style)
     if text_list:
         for i,text_el in enumerate(text_list):
-            text(x[i]+100000,y[i]+100000,text_el)
+            text(x[i]+100000,y[i]+100000,text_el.title())
 
 
 if __name__ == "__main__":
@@ -209,8 +256,12 @@ if __name__ == "__main__":
                              resolution = "c", style = 'bw', 
                              rolling_proj = False)
     
-    city = "Boulder, CO"
-    lon, lat = deg_min_sec2deg(get_coordinates_city(city))
+    city = "Dallas"
+    state = "TX"
+    country = "US"
+    lon, lat = get_coordinates_city(city, state, country)
+    if isinstance(lon, tuple):
+        lon, lat = deg_min_sec2deg((lon, lat))
     add_point_data(b, lat = [lat], lon = [lon], style = 'ro', 
                    text_list = [city])             
     show()
