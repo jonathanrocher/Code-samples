fcfe5cc334af1776d92327b872dc958c953ebb2f
Jonathan Rocher <jrocher@enthought.com>
2012-11-01 13:11:45 -0500

    Add comments about compilation.

--- llvm_clang_demo.c
+++ llvm_clang_demo.c
@@ -1,4 +1,7 @@
-
+/* This test file is used to demo how to compile C down to the LLVM Intermediate 
+Representation (LLVM-IR). To do so compile it with 
+clang llvm_clang_demo.c -c -emit-llvm -S -o llvm_clang_demo.ll
+*/
 
 int foo(int a, int b)
 {
