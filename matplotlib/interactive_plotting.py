9830a70e485144dd7d63b7fddf506f2a4bcea60f
Jonathan Rocher <jrocher@enthought.com>
2013-08-13 09:51:13 -0500

    Adding an animation in MPL simply using multiple calls to show.

---
+++ matplotlib/interactive_plotting.py
@@ -0,0 +1,25 @@
+""" Demo of making an animation with matplotlib. This is the simplest possible
+option: just redrawing different things on the same figure at a given interval.
+
+Alternatives to this require a timer or an instance of the animation subpackage
+in MPL.
+
+Note: this should be run outside of ipython, to avoid interference.
+"""
+import numpy as np
+from time import sleep
+from matplotlib.pyplot import show, ion, figure
+# Allow show() to be non-blocking
+ion()
+
+x = np.linspace(0, 4*np.pi)
+fig = figure()
+
+for phase in np.linspace(0, 2*np.pi):
+    y = np.sin(x+phase)
+    ax = fig.add_subplot(1,1,1)
+    line, = ax.plot(x, y)
+    # Force a redraw
+    ax.figure.canvas.draw()
+    sleep(0.5)
+    show()
